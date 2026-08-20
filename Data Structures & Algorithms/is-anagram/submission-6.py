d4413f4f70ea15c298c716fba1f11a13759951d2
TechElf <126956319+CodeTechDoll@users.noreply.github.com>
2026-08-20 13:26:30 -0700

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        counts = {}
+
+        for char_s, char_t in zip(s, t):
+            counts[char_s] = counts.get(char_s, 0) + 1
+            counts[char_t] = counts.get(char_t, 0) - 1
+
+        return all(count == 0 for count in counts.values())
